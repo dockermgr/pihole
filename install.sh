f9d4cff57058d95e5ef61387eadbb738878edd51
casjay <git-admin@casjaysdev.com>
2023-04-05 13:23:35 -0400

    🗃️  Committing everything that changed 🗃️

--- install.sh
+++ install.sh
@@ -1314,9 +1314,10 @@ EXECUTE_DOCKER_CMD="docker run -d $DOCKER_GET_OPTIONS $DOCKER_GET_CUSTOM $DOCKER
 # Run functions
 __container_import_variables "$CONTAINER_ENV_FILE_MOUNT"
 __dockermgr_variables >"$DOCKERMGR_CONFIG_DIR/env/$APPNAME"
-__custom_docker_env >"$DOCKERMGR_CONFIG_DIR/env/custom.$APPNAME"
 __dockermgr_password_variables >"$DOCKERMGR_CONFIG_DIR/secure/$APPNAME"
 chmod -f 600 "$DOCKERMGR_CONFIG_DIR/secure/$APPNAME"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+__custom_docker_env|tr ' ' '\n'|sed 's|--.*||g' |grep -v '^$' >"$DOCKERMGR_CONFIG_DIR/env/custom.$APPNAME"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Main progam
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
